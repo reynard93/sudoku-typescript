19bef38e3710675c21e4af7f45ecde58ff2d4edd
reynard <reynard93@gmail.com>
2020-06-21 10:39:26 +0800

    add typings for grid, set up styles for grid, scaffold code

---
+++ src/components/grid/index.tsx
@@ -0,0 +1,26 @@
+import React, { FC, Children } from 'react'
+
+import Block from './block'
+import { Container, Row } from './styles'
+
+const Grid: FC = () => {
+  // Other stuff on the way
+
+  return (
+    <Container data-cy="grid-container">
+      {Children.toArray(
+        [...Array(9)].map((_, rowIndex) => (
+          <Row data-cy="grid-row-container">
+            {Children.toArray(
+              [...Array(9)].map((_, colIndex) => (
+                <Block colIndex={colIndex} rowIndex={rowIndex} />
+              ))
+            )}
+          </Row>
+        ))
+      )}
+    </Container>
+  )
+}
+
+export default Grid
---
+++ src/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { ThemeProvider } from 'styled-components'
+
+import { Card, Content, Grid, Title } from 'components'
+import { unregister } from 'core'
+import { GlobalStyles, theme } from 'styles'
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <GlobalStyles />
+    <Content data-cy="content">
+      <Title data-cy="title">Sudoku</Title>
+      <Card data-cy="card">
+        <Grid />
+      </Card>
+    </Content>
+  </ThemeProvider>,
+  document.getElementById('root'),
+)
+
+unregister()
